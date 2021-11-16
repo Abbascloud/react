12a5c5f89bd41f8df67e52565f8ac0363824659d
Abbascloud <mamedovan91@yandex.ru>
2021-11-16 17:59:55 +0300

    hw_2

--- src/index.js
+++ src/index.js
@@ -1,36 +1,142 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 0,
+      author: "",
+      message: "",
+      sysAnswers: [],
+      messages: [],
+      count: 0,
+      isVisible: true,
+    };
+  }
 
-const Title = (props) => {
-  return <div>
-    <h1 class="title">{props.title}</h1>
-    <hr /></div>
+  clearInputs = () => {
+    this.setState({
+      author: "",
+      message: "",
+    });
+  };
+  sysAnswer = () => {
+    this.setState((state) => ({
+      sysAnswers: [
+        ...state.sysAnswers,
+        {
+          id: state.id + 1,
+          author: "system",
+          text: "hello",
+        },
+      ],
+    }));
+    this.getCount();
+  };
+  addMessage = () => {
+    this.setState((state) => ({
+      messages: [
+        ...state.messages,
+        {
+          id: state.id + 1,
+          author: state.author,
+          text: state.message,
+        },
+      ],
+    }));
+    this.getCount();
+    this.clearInputs();
+    setTimeout(this.sysAnswer, 1500);
+  };
 
-}
-
-const FunctionComponent = () => {
-  return (<div class="container">
-    <h1 class="title">FunctionComponent</h1>
-    <Title title="this props for Function component"/>
-  </div>)
-};
-
-
-class ClassComponent extends React.Component {
+  getCount = () => {
+    this.setState((state) => ({
+      count: state.messages.length,
+    }));
+  };
+  togle = () => {
+    this.setState(() => ({
+      isVisible: !this.state.isVisible,
+    }));
+  };
+  changeAuthor = (event) => {
+    this.setState(() => ({
+      author: event.target.value,
+    }));
+  };
+  changeText = (event) => {
+    this.setState(() => ({
+      message: event.target.value,
+    }));
+  };
   render() {
-    return (<div class="container">
-      <h1 class="title">ClassComp</h1>
-      <Title title="This props for ClassComponent" />
-    </div>)
+    const { messages, count, isVisible, author, message, sysAnswers } =
+      this.state;
+    return (
+      <div className="container">
+        <input
+          value={author}
+          onChange={this.changeAuthor}
+          className="author"
+          type="text"
+          placeholder="enter ur name"
+        />
+        <input
+          value={message}
+          onChange={this.changeText}
+          className="message"
+          type="text"
+          placeholder="enter ur message"
+        />
+        <button className="addMessageButton" onClick={this.addMessage}>
+          добавить
+        </button>
+        <div className="counterContainer">сообщений в чате: {count}</div>
+        {!isVisible && (
+          <button className="showMessageButton" onClick={this.togle}>
+            показать сообщения
+          </button>
+        )}
+        {isVisible && (
+          <button className="hideMessageButton" onClick={this.togle}>
+            скрыть сообщения
+          </button>
+        )}
+        {isVisible && (
+          <div className="messagesContainer">
+            {messages.map((message) => {
+              return (
+                <div key={message.id} className="messageContainer">
+                  <h3 className="messageAuthor">{message.author}</h3>
+                  <p className="messageText">{message.text}</p>
+
+                  <hr />
+                </div>
+              );
+            })}
+            {sysAnswers.map((sysAnswer) => {
+              return (
+                <div key={sysAnswer.id} className="messageContainer">
+                  <h3 className="messageAuthor">{sysAnswer.author}</h3>
+                  <p className="messageText">
+                    {sysAnswer.text}, {messages[messages.length - 1].author}!
+                  </p>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <ClassComponent />
-    <FunctionComponent/>
+    <Chat />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
